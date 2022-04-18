e0baa64020668f91cd991fc8608dc592cb8e5e3c
fauzanhalim <nurfauzanhalim@gmail.com>
2022-04-18 15:05:33 +0700

    first commit

---
+++ app/src/main/java/com/example/a10119226latihan1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.a10119226latihan1;
+
+//15 april 2022, 10119226 Nur Fauzan Halim IF6
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.a10119226latihan1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
